7164b655cc2cd92526743136de9a5889f877e1d7
swapna <masaliswapna023@gmail.com>
2024-02-07 12:42:34 +0530

    First commit

---
+++ functions.js
@@ -0,0 +1,26 @@
+//1} function as declaration
+function greet(a,b){
+    return a+b;
+}
+console.log(greet(10,20));
+
+//2]functionas as a expression
+const fun = function(name) {
+    console.log("Hello, " + name + "!");
+};
+fun(Swapna)
+//3]Arrow function
+const code = (name) => {
+    console.log("Hello, " + name + "!");
+};
+code(Javascript);
+//4] anonymous functions
+const blank = function(name) {
+    console.log("Hello, " + name + "!");
+};
+blank()
+
+setTimeout(function() {
+    console.log("Timeout expired!");
+}, 1000);
+
---
+++ variable.js
@@ -0,0 +1,34 @@
+// var a=10;
+// var a=10;
+// console.log(a);//10
+// function variable(){
+//     console.log(a);//10
+// }
+// variable()
+// {
+//     console.log(a);//10
+// }
+// var a=20;
+// console.log(a);//20
+// let a=10;
+
+// {
+//     console.log(a);
+//     let a=10;
+//     console.log(a);
+// }
+if(true){
+    var a=10
+    console.log(a);//10
+}
+console.log(a);//10
+function add(a,b){
+    console.log(a+b);//30
+}
+add(10,20);
+const b=10;
+console.log(b);//10
+function call(){
+    console.log(a);//10
+}
+call()
