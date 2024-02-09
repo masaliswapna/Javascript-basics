8d51d79bba829800f965ef3451ae8077fa610a9b
swapna masali <masaliswapna023@gmail.com>
2024-02-09 10:28:54 +0530

    first commit

--- arrays.js
+++ arrays.js
@@ -1,20 +1,70 @@
-//push
-const fruits = ['apple', 'banana', 'orange'];
-fruits.push('grape');
-console.log(fruits); // Output: ['apple', 'banana', 'orange', 'grape']
-//pop
-const numbers = [1, 2, 3, 4, 5];
-numbers.pop();
-console.log(numbers); // Output: [1, 2, 3, 4]
-//foreach
-const colors = ['red', 'green', 'blue'];
-colors.forEach((color,i)=>{
-console.log(color);
+//Creating an empty array
+let emptyarray=[];
+
+//creating an array with initialisinng values
+let numbers=[1,2,3,4,5];
+let fruits=['Apple', 'Mango', 'Banana', 'papaya'];
+
+//Accessing elements
+console.log(fruits[0]);//Apple
+console.log(fruits[1]);//Mango
+
+//accessing the length of an array
+console.log(fruits.length);//4
+
+//Adding elements at the end of an array
+fruits.push('Grapes');
+console.log(fruits);//[ 'Apple', 'Mango', 'Banana', 'papaya', 'Grapes' ]
+
+//Removing elements from the end of an array
+console.log(fruits.pop());//Grapes
+console.log(fruits);//[ 'Apple', 'Mango', 'Banana', 'papaya' ]
+
+//Manupulating an array
+//shift()
+console.log(fruits.shift());//Apple
+console.log(fruits);//[ 'Mango', 'Banana', 'papaya' ]
+
+//unshift()
+fruits.unshift('Orange','Watermelon');
+console.log(fruits);//[ 'Orange', 'Watermelon', 'Mango', 'Banana', 'papaya' ]
+
+//splice()
+fruits.splice(1,2,'kiwi', 'Pinapple');
+console.log(fruits);//[ 'Orange', 'kiwi', 'Pinapple', 'Banana', 'papaya' ]
+
+//Iterating over arrays
+
+
+// Using for loop
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+// Using forEach method
+fruits.forEach(function(fruit) {
+    console.log(fruit);
 });
-//filter
-const scores = [85, 90, 70, 95, 60];
-const passed = scores.filter(score => score >= 75);
-console.log(passed); // Output: [85, 90, 95]
+
+// Using for...of loop
+for (let fruit of fruits) {
+    console.log(fruit);
+}
+
+//Searching and filtering
+
+
+// Finding an element's index
+let index = numbers.indexOf(3); // index: 2
+console.log(index);
+
+// Filtering elements
+let evenNumbers = numbers.filter(function(number) {
+    return number % 2 === 0;
+}); 
+console.log(evenNumbers);   // evenNumbers: [2, 4]
+
+
 //map
 const num = [1, 2, 3, 4, 5];
 const squaredNumbers = num.map(num => num * num);
